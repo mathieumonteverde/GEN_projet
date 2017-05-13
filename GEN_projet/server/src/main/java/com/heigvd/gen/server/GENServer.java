d1d5524a3edac980b3d7db8050a4a9d33f1f1c70
Mathieu <mathieu.monteverde@gmail.com>
2017-05-13 16:59:28 +0200

    Cleaned GENServer

--- GEN_projet/server/src/main/java/com/heigvd/gen/server/GENServer.java
+++ GEN_projet/server/src/main/java/com/heigvd/gen/server/GENServer.java
@@ -43,67 +43,6 @@ public class GENServer {
       // Create the TCPServer
       tcpServer = new TCPServer(this, 2525);
       new Thread(tcpServer).start();
-
-      DBInterface dbi = new DBInterface();
-      boolean b1;
-      try {
-         b1 = dbi.connectUser("Valomat", "1234");
-         System.out.println(b1);
-      } catch (SQLException ex) {
-         Logger.getLogger(GENServer.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      boolean b2;
-      try {
-         b2 = dbi.connectUser("Valomat", "lol");
-         System.out.println(b2);
-      } catch (SQLException ex) {
-         Logger.getLogger(GENServer.class.getName()).log(Level.SEVERE, null, ex);
-      }
-
-      try {
-         dbi.registerUser("Mat", "1234");
-         System.out.println("User successfully registered!");
-         dbi.registerUser("Mat", "1234");
-      } catch (UsedUsernameException e) {
-         System.out.println("User already exists");
-      } catch (SQLException ex) {
-         Logger.getLogger(GENServer.class.getName()).log(Level.SEVERE, null, ex);
-      }
-
-      try {
-         dbi.addScore("Fire Race", 1, 6900, "1993-03-03", "Valomat");
-         dbi.addScore("Fire Race", 2, 7200, "1993-03-03", "Valomat");
-         dbi.addScore("Fire Race", 3, 8799, "1993-03-03", "Valomat");
-      } catch (SQLException ex) {
-         Logger.getLogger(GENServer.class.getName()).log(Level.SEVERE, null, ex);
-      }
-
-      try {
-         System.out.println("Changing from 1234 to 4567");
-         dbi.changeUserPassword("Valomat", "1234", "4567");
-         System.out.println("Trying to connect with 4567");
-         b1 = dbi.connectUser("Valomat", "4567");
-         System.out.println(b1);
-         System.out.println("Changing from 1234 to LOL");
-         dbi.changeUserPassword("Valomat", "1234", "LOL");
-
-      } catch (BadAuthentificationException ex) {
-         System.out.println("Couldn't change password because of bad authentification.");
-      } catch (SQLException ex) {
-         System.out.println(ex.getMessage());
-         try {
-            b1 = dbi.connectUser("Valomat", "4567");
-            System.out.println(b1);
-         } catch (SQLException ee) {
-            Logger.getLogger(GENServer.class.getName()).log(Level.SEVERE, null, ee);
-         }
-      }
-
-      try {
-         dbi.changeUserRole("Valomat", UserPrivilege.Privilege.SUPER_ADMIN);
-      } catch (SQLException ex) {
-         Logger.getLogger(GENServer.class.getName()).log(Level.SEVERE, null, ex);
-      }
    }
 
    /**
