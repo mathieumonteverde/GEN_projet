32e51de3a76f5894f82e3677ad98e85974aa1f00
mathieumonteverde <mathieu.monteverde@gmail.com>
2017-06-09 19:01:42 +0200

    Started working on tcp communication in libgdx project

--- GEN_projet/core/src/com/heigvd/gen/states/UserConnectionState.java
+++ GEN_projet/core/src/com/heigvd/gen/states/UserConnectionState.java
@@ -13,50 +13,88 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.heigvd.gen.client.TCPClient.TCPClient;
+import com.heigvd.gen.client.TCPClient.TCPClientListener;
+import com.heigvd.gen.client.TCPClient.TCPErrors;
 import com.heigvd.gen.guicomponent.GuiComponent;
+import com.heigvd.gen.protocol.tcp.message.TCPRoomInfoMessage;
+import com.heigvd.gen.protocol.tcp.message.TCPRoomMessage;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author mathieu
  */
-public class UserConnectionState extends State {
+public class UserConnectionState extends State implements TCPClientListener {
 
+   // Stage to display elements
    private Stage stage;
-   TextButton button;
-   TextField username;
-   TextField password;
+
+   // Buttons to manage the state
+   private TextButton connect;
+   private TextButton register;
+   private TextField username;
+   private TextField password;
+
+   // TCP client to communicate with server
+   private TCPClient tcpClient;
+
+   private final GameStateManager g;
 
    public UserConnectionState(GameStateManager gsm) {
       super(gsm);
 
-      final GameStateManager g = gsm;
-      
+      g = gsm;
+
       // Créer une stage pour contenir les éléments
       stage = new Stage();
       Gdx.input.setInputProcessor(stage);
-      
+
       // Créer les champs textes
       username = GuiComponent.createTextField("Username...");
       password = GuiComponent.createTextField("Password...");
-      
-      // Créer le bouton de connection
-      button = GuiComponent.createButton("Connect", 200, 60);
-      button.addListener(new ChangeListener() {
+
+      // Créer le bouton de d'inscription
+      register = GuiComponent.createButton("Register", 200, 60);
+      register.addListener(new ChangeListener() {
          @Override
          public void changed(ChangeEvent event, Actor actor) {
-            g.set(new MainMenuState(g));
+            tcpClient.registerUser(username.getText(), username.getText());
          }
       });
-      
+
+      // Créer le bouton de connection
+      connect = GuiComponent.createButton("Connect", 200, 60);
+      connect.addListener(new ChangeListener() {
+         @Override
+         public void changed(ChangeEvent event, Actor actor) {
+            tcpClient.connectUser(username.getText(), username.getText());
+         }
+      });
+
       // Utiliser l'utilitaire maison pour centrer les éléments et offset
       GuiComponent.centerGuiComponent(username, stage, 0, 0);
       GuiComponent.centerGuiComponent(password, stage, 0, -50);
-      GuiComponent.centerGuiComponent(button, stage, 0, -120);
-      
+      GuiComponent.centerGuiComponent(connect, stage, 0, -120);
+      GuiComponent.centerGuiComponent(register, stage, 0, -190);
+
       // Ajouter au stage
       stage.addActor(username);
       stage.addActor(password);
-      stage.addActor(button);
+      stage.addActor(connect);
+      stage.addActor(register);
+
+      try {
+         // Connect to the db
+         tcpClient = new TCPClient("localhost", 2525, this);
+         new Thread(tcpClient).start();
+
+      } catch (IOException ex) {
+         Logger.getLogger(UserConnectionState.class.getName()).log(Level.SEVERE, null, ex);
+      }
    }
 
    @Override
@@ -76,6 +114,44 @@ public class UserConnectionState extends State {
    @Override
    public void dispose() {
       stage.dispose();
+   }
+
+   @Override
+   public void listRooms(List<TCPRoomMessage> rooms) {
+   }
+
+   @Override
+   public void joinRoom() {
+   }
+
+   @Override
+   public void roomInfo(TCPRoomInfoMessage msg) {
+   }
+
+   @Override
+   public void connectUser() {
+      Gdx.app.postRunnable(new Runnable() {
+         @Override
+         public void run() {
+            g.set(new MainMenuState(g));
+         }
+      });
+
+   }
+
+   @Override
+   public void registerUser() {
+      Gdx.app.postRunnable(new Runnable() {
+         @Override
+         public void run() {
+            g.set(new MainMenuState(g));
+         }
+      });
+   }
+
+   @Override
+   public void errorNotification(TCPErrors.Error error) {
+
    }
 
 }
