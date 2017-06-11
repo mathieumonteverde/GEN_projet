a12c7d7971cf1a98712a0c39105abb193fb6aca8
mathieumonteverde <mathieu.monteverde@gmail.com>
2017-06-11 21:33:50 +0200

     Added server connection handling

--- GEN_projet/core/src/com/heigvd/gen/states/HomeScreenState.java
+++ GEN_projet/core/src/com/heigvd/gen/states/HomeScreenState.java
@@ -5,16 +5,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.heigvd.gen.Player;
 import com.heigvd.gen.RaceSimulation;
 import com.heigvd.gen.client.TCPClient.TCPClient;
 import com.heigvd.gen.guicomponent.GuiComponent;
+import com.heigvd.gen.useraccess.UserPrivilege;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Timer;
 
 /**
  *
@@ -25,9 +28,9 @@ public class HomeScreenState extends State {
    private GameStateManager gsm;
 
    private Stage stage;
-   
+
    private TCPClient tcpClient;
-   
+
    private Texture background;
 
    public HomeScreenState(GameStateManager gsm) {
@@ -38,7 +41,7 @@ public class HomeScreenState extends State {
       int gameWidth = Gdx.graphics.getWidth();
       int gameHeight = Gdx.graphics.getHeight();
       stage = new Stage(new StretchViewport(gameWidth, gameHeight));
-      
+
       background = new Texture(Gdx.files.internal("GEN_home_bg.jpg"));
 
       final TextField address = GuiComponent.createTextField("Server IP address");
@@ -55,11 +58,30 @@ public class HomeScreenState extends State {
                // Connect to the db
                tcpClient = new TCPClient(address.getText(), 2525, null);
                new Thread(tcpClient).start();
-               
+
                HomeScreenState.this.gsm.set(new UserConnectionState(HomeScreenState.this.gsm, tcpClient));
 
             } catch (IOException ex) {
-               Logger.getLogger(UserConnectionState.class.getName()).log(Level.SEVERE, null, ex);
+
+               final Dialog endDialog = new Dialog("Error when trying to connect to the server...", GuiComponent.getSkin()) {
+                  protected void result(Object object) {
+
+                     Gdx.app.postRunnable(new Runnable() {
+                        @Override
+                        public void run() {
+                           hide();
+                        }
+                     });
+                  }
+               };
+               endDialog.button("Try Again...", 1L);
+
+               Gdx.app.postRunnable(new Runnable() {
+                  @Override
+                  public void run() {
+                     endDialog.show(stage);
+                  }
+               });
             }
          }
       });
@@ -80,8 +102,9 @@ public class HomeScreenState extends State {
    @Override
    public void render(SpriteBatch sb) {
       sb.begin();
-      sb.draw(background,0,0, RaceSimulation.WIDTH, RaceSimulation.HEIGHT);
+      sb.draw(background, 0, 0, RaceSimulation.WIDTH, RaceSimulation.HEIGHT);
       sb.end();
+      stage.act();
       stage.draw();
    }
 
