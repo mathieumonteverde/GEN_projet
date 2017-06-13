ae97de9a3d3b1afdec127dac976a0f098b4b932b
mathieumonteverde <mathieu.monteverde@gmail.com>
2017-06-13 15:50:47 +0200

    Added comments

--- GEN_projet/server/src/main/java/com/heigvd/gen/DBInterface/exception/UsedUsernameException.java
+++ GEN_projet/server/src/main/java/com/heigvd/gen/DBInterface/exception/UsedUsernameException.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.heigvd.gen.DBInterface.exception;
 
 /**
- *
- * @author mathieu
+ * Custom exception to represent a used username error when trying to 
+ * register a user.
  */
 public class UsedUsernameException extends Exception {
    public UsedUsernameException() {
